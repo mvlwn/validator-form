cbade4c0e11663c631f94cd7c63723f00ca5269f
Martijn van Leeuwen <m.vanleeuwen@websend.nl>
2011-09-21 21:59:22 +0200

    small additions

--- test/test.js
+++ test/test.js
@@ -1,10 +1,22 @@
 test("Constructor", function() {
-  var form = $("#myform");
-  ok( form.wsValidate(), "OrderForm should validate");
+  var validator = $("#myform").wsValidator();
+  console.log(validator);
+  ok( validator, "OrderForm should validate");
 });
 
-test("Constructor::AddRule", function() {
-  var form = $("#myform").wsValidate();
-  form.addRule("test", function(){});
-  ok( (form.rules["test"] != undefined) , "addRule should add a rule option");
-});
+// test("Constructor::AddRule", function() {
+//   var validator = $("#myform").wsValidate();
+//   validator.addRule("test", function(){});
+//   ok( (validator.rules["test"] != undefined) , "addRule should add a rule option");
+// });
+// 
+// test("Constructor::Elements", function() {
+//   var validator = $("#myform").wsValidate();
+//   ok( ($.type(validator.elements) == "array") , "elements should be an array (" + $.type(validator.elements) + ")");
+// });
+// 
+// 
+// test("Attributes", function(){
+//   var name = $("#myform [name=name]");
+//   ok( name.attr("data-validate").length > 0, "data-validate must be present");
+// });
